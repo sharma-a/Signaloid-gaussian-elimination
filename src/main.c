e9e7f5c551342927cfa9b994d69c2b927ba1dfa3
Ankur Sharma <sharma.a1987@gmail.com>
2022-10-08 23:43:33 +0530

    GaussianRVsTese

--- src/main.c
+++ src/main.c
@@ -3,93 +3,63 @@
 #include <time.h>
 #include <stdlib.h>
 #include <uncertain.h>
+#include <math.h>
 
-
-
-
-double uniformRandom(double min, double max){
-    return min+(max-min)*((double)random()/RAND_MAX);
-}
 
 
 int main(int argc, char *argv[]){
-    int N=atoi(argv[1]);
-
-   srandom(time(NULL));
-   Matrix A,B;
-   int nRow, nCol;
-   nRow=N;
-   nCol=N+1;
-   initialiseMatrix(&A,nRow,nCol);
-   initialiseMatrix(&B,nRow,nCol);
-
+    int N;
+    Matrix A,B;
+    int nRow, nCol;
     FILE *fptr;
-   if(!(fptr=fopen("mnt/mnt/Abinp","r"))) {printf ("\nFile Error.\n"); return -1;}
-       
-
-   for(int i=0;i<nRow;++i){
+    if(!(fptr=fopen("mnt/Abinp","r"))) {printf ("\nFile Error.\n"); return -1;}
+    fscanf(fptr,"%d\n",&N);
+    nRow=N;
+    nCol=N+1;
+    initialiseMatrix(&A,nRow,nCol);
+    initialiseMatrix(&B,nRow,nCol);
+    for(int i=0;i<nRow;++i){
       for(int j=0;j<nCol;++j){
           double mu;
           double sigma;
-
           fscanf(fptr,"%lf,%lf\n",&mu,&sigma);
           *at(&A,i,j)=libUncertainDoubleGaussDist(mu,sigma);
           *at(&B,i,j)=*at(&A,i,j);
       }
-   }
+    }
+    fclose(fptr);
+
+
+    printMatrix(&A);
+    double* soln=(double*) malloc(N*sizeof(double));
+    solveEqnGaussJordan(&A,soln);
 
 
 
-
-   fclose(fptr);
-
- 
-
-   double* soln=(double*) malloc(N*sizeof(double));
-
-   printMatrix(&A);
-
-   solveEqnGaussJordan(&A,soln);
-
-
-   //printf("After Gauss Jordan:\n");
-   //printMatrix(&A);
-
-   if(!(fptr=fopen("mnt/Absoln","w"))) {printf ("\nFile Error.\n"); return -1;}
-
-
-   printf("Soln:\n");
-   for(int i=0;i<N;++i){
+    if(!(fptr=fopen("mnt/Absoln","w"))) {printf ("\nFile Error.\n"); return -1;}
+    printf("Soln:\n");
+    for(int i=0;i<N;++i){
        soln[i]=*at(&A,i,nCol-1)/(*at(&A,i,i));
        printf("%.4lf, %.4lf, %.4lf\n",soln[i],libUncertainDoubleNthMoment(soln[i],1), libUncertainDoubleNthMoment(soln[i],2));
        fprintf(fptr,"%.4lf, %.4lf, %.4lf\n",soln[i],libUncertainDoubleNthMoment(soln[i],1), libUncertainDoubleNthMoment(soln[i],2));
-   }
-   printf("\n");
-   fclose(fptr);
+    }
+    printf("\n");
+    fclose(fptr);
 
 
-   double* solnB=(double*) malloc(N*sizeof(double));
+    double* solnB=(double*) malloc(N*sizeof(double));
+    solveEqn(&B,solnB);
+    printf("Soln Triangulise:\n");
+    for(int i=0;i<N;++i){
+       printf("%.4lf, %.4lf, %.4lf\n",solnB[i],libUncertainDoubleNthMoment(solnB[i],1), libUncertainDoubleNthMoment(solnB[i],2));
+    }
+    printf("\n");
 
-   solveEqn(&B,solnB);
+    freeMatrix(&B);
+    free(solnB);
+    freeMatrix(&A);
+    free(soln);
 
-   
+    return 0;
 
-   printf("Soln Triangulise:\n");
-   for(int i=0;i<N;++i){
-       printf("%7.5lf  ",solnB[i]);
-   }
-   printf("\n");
-
-
-
-
-   freeMatrix(&B);
-   free(solnB);
-
-
-   freeMatrix(&A);
-   free(soln);
-
-   return 0;
-   
 }
