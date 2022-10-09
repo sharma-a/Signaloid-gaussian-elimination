d6ebd33ef0c2f71222fb14892ee0e8df2a52f91f
Ankur Sharma <sharma.a1987@gmail.com>
2022-10-09 16:32:01 +0530

    TestReadFile

--- src/main.c
+++ src/main.c
@@ -1,13 +1,15 @@
-#include "matrix.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <uncertain.h>
+#include "Matrix.h"
 
-int strToDoubleArray(char* str, double* ret){
+int readLineFromFile(char* str, char* type, double* ret){
     char *token;
     int nSamp=0;
     token=strtok(str,",");
+    strcpy(type,token);
+    token=strtok(NULL,",");
     while(token!=NULL){
         ret[nSamp++]=atof(token);
         token=strtok(NULL,","); 
@@ -17,50 +19,50 @@ int strToDoubleArray(char* str, double* ret){
 
 
 
-int main(){
+Matrix* readFromFile(char* filename){
     FILE *fptr;
-    if(!(fptr=fopen("mnt/Abinp","r"))) {printf ("\nFile Error.\n"); return -1;}
+    if(!(fptr=fopen(filename,"r"))) {printf ("\nFile Error.\n"); return NULL;}
     int nRow, nCol;
     fscanf(fptr,"%d\n",&nRow);
     nCol=nRow+1;
-    char line[300];
     double samples[30];
-
     int nSamp;
+    char type[20];
+    printf("nRow is %d\n",nRow);
+    Matrix* ret;
+    ret=(Matrix*) malloc(sizeof(Matrix));
+    initialiseMatrix(ret,nRow,nCol);
 
-    Matrix A;
-    initialiseMatrix(&A, nRow, nCol);
     for(int i=0;i<nRow;++i){
         for(int j=0;j<nCol;++j){
-            //getline(&line, &len, fptr);
+            char line[300];
+            char type[20];
             fgets(line, 300, fptr);
-            nSamp=strToDoubleArray(line, samples);
-//             for(int k=0;k<nSamp;++k)
-//                printf("%lf\t",samples[k]);
- //           printf("\n");
-            *at(&A,i,j)=libUncertainDoubleDistFromSamples(samples,nSamp);
+            nSamp=readLineFromFile(line, type, samples);
+            if(strcmp(type,"gauss")==0){
+                *at(ret,i,j=libUncertainDoubleGaussDist(samples[0],samples[1]);
+            }
+            else if(strcmp(type,"unif")==0){
+                *at(ret,i,j=libUncertainDoubleUniformDist(samples[0],samples[1]);
+            }
+            else if(strcmp(type,"exp")==0){
+                *at(ret,i,j=libUncertainDoubleExponentialDist(samples[0]);
+            }
+            else if(strcmp(type,"custom")==0){
+                if(nSamp==1) *at(ret,i,j)=samples[0];
+                else *at(ret,i,j)=libUncertainDoubleDistFromSamples(samples,nSamp);
+
+            }
         }
     }
 
     fclose(fptr);
+    return ret;
+}
 
 
-   double* soln=(double*) malloc(nRow*sizeof(double));
-
-   printMatrix(&A);
-
-   solveEqn(&A, soln);
-    for(int i=0;i<nRow;++i){
-       printf("%.4lf, %.4lf, %.4lf\n",soln[i],libUncertainDoubleNthMoment(soln[i],1), libUncertainDoubleNthMoment(soln[i],2));
-       fprintf(fptr,"%.4lf, %.4lf, %.4lf\n",soln[i],libUncertainDoubleNthMoment(soln[i],1), libUncertainDoubleNthMoment(soln[i],2));
-    }
- 
-   printf("\n");
-
-
-   freeMatrix(&A);
-   free(soln);
-
-   return 0;
-   
+int main(){
+    char* filename="mnt/Mixinput";
+    Matrix *A=readFromFile(filename);
+    printMatrix(A);
 }
