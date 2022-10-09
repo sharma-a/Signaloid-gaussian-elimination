bfa5e22fb41c9b9c751316e234fb8e162f67cbd9
Ankur Sharma <sharma.a1987@gmail.com>
2022-10-09 13:48:42 +0530

    TestCustom

--- src/main.c
+++ src/main.c
@@ -1,90 +1,63 @@
 #include "matrix.h"
 #include <stdio.h>
-#include <time.h>
+#include <string.h>
 #include <stdlib.h>
-#include <uncertain.h>
-#include <math.h>
+
+int strToDoubleArray(char* str, double* ret){
+    char *token;
+    int nSamp=0;
+    token=strtok(str,",");
+    while(token!=NULL){
+        ret[nSamp++]=atof(token);
+        token=strtok(NULL,","); 
+    }
+    return nSamp;
+}
 
 
 
-int main(int argc, char *argv[]){
-    Matrix A,B;
+int main(){
+
+    FILE* fptr;
+    fptr=fopen("Abinp","r");
     int nRow, nCol;
-    int N=3;
-    nRow=N;
-    nCol=N+1;
-    initialiseMatrix(&A,nRow,nCol);
-    initialiseMatrix(&B,nRow,nCol);
-    FILE *fptr;
-   
-   {
-       double sampleA00[]={3.2,2.8,3.3};
-       double sampleA01[]={13.8,10.5,12.3};
-       double sampleA02[]={5.3,5.8,5.0};
-       double sampleA10[]={-3,-3.8,-3.5};
-       double sampleA11[]={23.2,22.8,23.3};
-       double sampleA12[]={-7.2,7.8,8.1};
-       double sampleA20[]={10.2,12.8,10.6};
-       double sampleA21[]={0.8,1.2,0.8};
-       double sampleA22[]={-3.2,-2.8,-3.3};
-       double sampleb0[]={43.2,40.2,46.3};
-       double sampleb1[]={23.2,22.8,23.3};
-       double sampleb2[]={-13.2,-12.8,-15.0};
-       *at(&A,0,0) = libUncertainDoubleDistFromSamples(sampleA00,sizeof(sampleA00)/sizeof(double));
-       *at(&A,0,1) = libUncertainDoubleDistFromSamples(sampleA01,sizeof(sampleA01)/sizeof(double));
-       *at(&A,0,2) = libUncertainDoubleDistFromSamples(sampleA02,sizeof(sampleA02)/sizeof(double));
-       *at(&A,1,0) = libUncertainDoubleDistFromSamples(sampleA10,sizeof(sampleA10)/sizeof(double));
-       *at(&A,1,1) = libUncertainDoubleDistFromSamples(sampleA11,sizeof(sampleA11)/sizeof(double));
-       *at(&A,1,2) = libUncertainDoubleDistFromSamples(sampleA12,sizeof(sampleA12)/sizeof(double));
-       *at(&A,2,0) = libUncertainDoubleDistFromSamples(sampleA20,sizeof(sampleA20)/sizeof(double));
-       *at(&A,2,1) = libUncertainDoubleDistFromSamples(sampleA21,sizeof(sampleA21)/sizeof(double));
-       *at(&A,2,2) = libUncertainDoubleDistFromSamples(sampleA22,sizeof(sampleA22)/sizeof(double));
+    fscanf(fptr,"%d\n",&nRow);
+    nCol=nRow+1;
+    char *line=NULL;
+    double samples[30];
 
-       *at(&A,0,3) = libUncertainDoubleDistFromSamples(sampleb0,sizeof(sampleb0)/sizeof(double));
-       *at(&A,1,3) = libUncertainDoubleDistFromSamples(sampleb1,sizeof(sampleb1)/sizeof(double));
-       *at(&A,2,3) = libUncertainDoubleDistFromSamples(sampleb2,sizeof(sampleb2)/sizeof(double));
-   }
+    int nSamp;
+    size_t len;
 
- for(int i=0;i<nRow;++i){
-      for(int j=0;j<nCol;++j){
-          *at(&B,i,j)=*at(&A,i,j);
-      }
+    Matrix A;
+    initialiseMatrix(&A, nRow, nCol);
+    for(int i=0;i<nRow;++i){
+        for(int j=0;j<nCol;++j){
+            getline(&line, &len, fptr);
+            nSamp=strToDoubleArray(line, samples);
+            *at(&A,i,j)=libUncertainDoubleDistFromSamples(samples,nSamp);
+        }
     }
 
-
-   
-
-
-
-    printMatrix(&A);
-    double* soln=(double*) malloc(N*sizeof(double));
-    solveEqnGaussJordan(&A,soln);
-
-
-
-    if(!(fptr=fopen("mnt/Absoln","w"))) {printf ("\nFile Error.\n"); return -1;}
-    printf("Soln:\n");
-    for(int i=0;i<N;++i){
-       printf("%.4lf, %.4lf, %.4lf\n",soln[i],libUncertainDoubleNthMoment(soln[i],1), libUncertainDoubleNthMoment(soln[i],2));
-       fprintf(fptr,"%.4lf, %.4lf, %.4lf\n",soln[i],libUncertainDoubleNthMoment(soln[i],1), libUncertainDoubleNthMoment(soln[i],2));
-    }
-    printf("\n");
     fclose(fptr);
 
 
-    double* solnB=(double*) malloc(N*sizeof(double));
-    solveEqn(&B,solnB);
-    printf("Soln Triangulise:\n");
-    for(int i=0;i<N;++i){
-       printf("%.4lf, %.4lf, %.4lf\n",solnB[i],libUncertainDoubleNthMoment(solnB[i],1), libUncertainDoubleNthMoment(solnB[i],2));
+   double* soln=(double*) malloc(N*sizeof(double));
+
+   printMatrix(&A);
+
+   solveEqn(&A, soln);
+    for(int i=0;i<nRow;++i){
+       printf("%.4lf, %.4lf, %.4lf\n",soln[i],libUncertainDoubleNthMoment(soln[i],1), libUncertainDoubleNthMoment(soln[i],2));
+       fprintf(fptr,"%.4lf, %.4lf, %.4lf\n",soln[i],libUncertainDoubleNthMoment(soln[i],1), libUncertainDoubleNthMoment(soln[i],2));
     }
-    printf("\n");
+ 
+   printf("\n");
 
-    freeMatrix(&B);
-    free(solnB);
-    freeMatrix(&A);
-    free(soln);
 
-    return 0;
+   freeMatrix(&A);
+   free(soln);
 
+   return 0;
+   
 }
