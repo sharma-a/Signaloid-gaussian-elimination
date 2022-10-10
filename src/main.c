6523afb0abeb32c1f258a3e209c8053e35679941
sharma-a <sharma.a1987@gmail.com>
2022-10-10 09:19:30 +0530

    Update main.c

--- src/main.c
+++ src/main.c
@@ -9,6 +9,9 @@ int main(int argc, char* argv[]){
     char* filename=argv[1];
     Matrix *A=readFromFile(filename);//for method 1
     Matrix *Acopy=createCopyMatrix(A);//for method 2
+    //Note these matrices are Augmented matrices.
+    //i.e. for solving Ax=b we need the matrix [A|b] which has b appended as another column to A
+    //the i/o function produces and the solution functions work on such augmented matrices.
 
     int nRow=A->nRow;
 
